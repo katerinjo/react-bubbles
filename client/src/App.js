5c6717882da3c3f7ed2222780ce26c07a1fbe006
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-23 09:35:47 -0700

    abort/undo refactoring and styling; Login's structure isn't ideal; redirect works

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import BubblePage from './components/BubblePage';
+import './styles.scss';
 
 function App() {
-  const [colorList, setColorList] = useState([]);
+  // const [colorList, setColorList] = useState([]);
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+      <div className='App'>
+        <h1>oOo Bubbles oOo</h1>
+        <Route exact path='/' component={Login} />
+        <Route path='/bubbles' component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 
 import LoginForm from './LoginForm';
 
-const Login = () => {
+const Login = ({ history }) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const loginRequest = credentials => {
     axios.post('http://localhost:5000/api/login', credentials)
       .then(res => {
-        console.log(res)
         localStorage.setItem('token', res.data.payload);
+        console.log(history)
+        history.push('/bubbles')
       })
       .catch(console.log);
   }
   return (
     <>
-      <h1>Bubble App</h1>
       <LoginForm subFun={loginRequest} />
     </>
   );
