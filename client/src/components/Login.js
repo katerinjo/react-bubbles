4cfd576047b0ab061b15625bac49c2526bad7437
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-23 08:53:16 -0700

    form displays; yup errors not working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+
+import LoginForm from './LoginForm';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const loginRequest = credentials => {
+    axios.post('http://localhost:3333/api/login', credentials)
+      .then(res => {
+        console.log(res)
+        // localStorage.setItem('token', res.data.payload);
+      })
+      .catch(console.log);
+  }
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <h1>Bubble App</h1>
+      <LoginForm subFun={loginRequest} />
     </>
   );
 };
